1645be32f6af38edb1448f53f50a6d0c2ac3e37f
chaehyeo-n <parkche03@naver.com>
2024-12-02 22:41:55 +0900

    🐛 fix: TTS api speechsynthesis로 변경

--- public/background.js
+++ public/background.js
@@ -1,11 +1,15 @@
 // content.js로부터 메시지를 받아 TTS로 읽어주는 기능
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.text) {
-    chrome.tts.speak(message.text, {
-      rate: 1.0,
-      pitch: 1.0,
-      volume: 1.0,
-      lang: 'ko-KR',
-    });
+    const utterance = new SpeechSynthesisUtterance(message.text);
+
+    // 음성 설정
+    utterance.lang = 'ko-KR'; // 한국어 설정
+    utterance.rate = 1.0; // 속도
+    utterance.pitch = 1.0; // 음정
+    utterance.volume = 1.0; // 볼륨
+
+    speechSynthesis.cancel();
+    speechSynthesis.speak(utterance);
   }
 });
